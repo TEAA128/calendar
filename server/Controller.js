eea3d1f1aadb64a97c8b93c98f23b10bad22e479
tegshee <tegshbayar@gmail.com>
2020-07-08 00:09:05 -0700

    Add post, delete requests

--- server/Controller.js
+++ server/Controller.js
@@ -19,7 +19,31 @@ const patch = function (req, res) {
     });
 };
 
+const post = function (req, res) {
+  const placeID = req.params.placeID;
+  const obj = req.body;
+  Calendar.save({ id: placeID }, { $push: { bookings: obj } })
+    .exec((err, data) => {
+      if (err) res.sendStatus(400);
+      res.send(data);
+    });
+};
+
+const delete = function (req, res) {
+  const placeID = req.params.placeID;
+  const obj = req.body;
+  Calendar.findByIdAndRemove({ id: placeID })
+    .exec((err, data) => {
+      if (err) res.sendStatus(400);
+      res.sendStatus(200);
+    });
+};
+
+
 module.exports = {
   find,
-  patch
+  patch,
+  post,
+  put,
+  delete
 }
