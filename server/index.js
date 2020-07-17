26454dddb7e4f1f173497730b1e2962027adf90c
tegshee <tegshbayar@gmail.com>
2020-07-17 16:08:14 -0700

    Complete K6 test locally

--- server/index.js
+++ server/index.js
@@ -1,35 +1,48 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const pool = require('../database/index.js');
 
-const app = express();
-const port = 3001;
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    // Create a worker
+    cluster.fork();
+  }
+} else {
+  // Workers share the TCP connection in this server
+  const app = express();
 
-app.use(cors());
+  // All workers use this port
+  const port = 3001;
 
-app.use('/calendar/', express.static(path.join(__dirname, '../client/dist/')));
+  app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+  app.use('/calendar/', express.static(path.join(__dirname, '../client/dist/')));
 
-// get request
-app.get('/api/calendar/:placeId', (req, res) => {
-  const { placeId } = req.params;
-  const query = `SELECT * FROM bookings WHERE booking_id_serial = ${placeId}`;
-  pool.connect((err, client, done) => {
-    if (err) throw err;
-    client.query(query, (error, result) => {
-      done();
-      if (err) {
-        res.status(404).send(error.stack);
-      } else {
-        res.status(200).json(result.rows[0]);
-      }
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  // get request
+  app.get('/api/calendar/:placeId', (req, res) => {
+    const { placeId } = req.params;
+    const query = `SELECT * FROM bookings WHERE booking_id_serial = ${placeId}`;
+    pool.connect((err, client, done) => {
+      if (err) throw err;
+      client.query(query, (error, result) => {
+        done();
+        if (err) {
+          res.status(404).send(error.stack);
+        } else {
+          res.status(200).json(result.rows[0]);
+        }
+      });
     });
   });
-});
+  app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+}
 
 // patch request
 // app.patch('/api/calendar/:placeID', () => {console.log('This is PATCH')});
@@ -39,5 +52,3 @@ app.get('/api/calendar/:placeId', (req, res) => {
 
 // // delete request
 // app.delete('/api/calendar/:placeID', () => {console.log('This is delete')});
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
--- server/script.js
+++ server/script.js
@@ -2,11 +2,21 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 800,
-  duration: '10s',
+  vus: 2000,
+  duration: '30s',
 };
 
+// export const options = {
+//   stages: [
+//     { duration: '10s', target: 50 },
+//     { duration: '15s', target: 100 },
+//     { duration: '20s', target: 500 },
+//     { duration: '10s', target: 0 },
+//   ],
+// };
+
 export default function() {
-  http.get('http://test.k6.io');
+  const placeId = Math.floor(Math.random() * (10000000)) + 1;
+  http.get(`http://localhost:3001/api/calendar/${placeId}`);
   sleep(1);
 }
