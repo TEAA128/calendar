9ae1827d84fd9e6a1a1758568dcf3837b11befea
tegshee <tegshbayar@gmail.com>
2020-07-17 22:35:38 -0700

    Setup New Relic

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports.keys = {
+  newRelicKey: '3fef82dfa5cd1c6eeab5d1cb16a585044762NRAL',
+};
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const express = require('express');
